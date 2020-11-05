3e0ffb9bc91a2b9e02e36371d65b3db50db3fcd6
Zaboryev Sergey <sergynia88@mail.ru>
2020-11-05 13:58:03 +0300

    Инфраструктура папок

---
+++ AppForChildren(MVVM)/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace AppForChildren_MVVM_
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
