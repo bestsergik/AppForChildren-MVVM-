bea309196bbb34d24637391e43d4650da8683871
Zaboryev Sergey <sergynia88@mail.ru>
2020-11-05 15:02:21 +0300

    Заголовок окна

---
+++ AppForChildren(MVVM)/ViewModels/Base/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppForChildren_MVVM_.ViewModels.Base
+{
+   internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title;
+
+
+        /// <summary>Заголовок окна </summary>
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if (Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChancged();
+            //    Set(ref _Title, value);
+            //}
+
+            set => Set(ref _Title, value);
+        }
+    }
+}
