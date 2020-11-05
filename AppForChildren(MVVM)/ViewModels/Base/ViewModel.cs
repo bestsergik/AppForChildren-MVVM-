78858d6c0ba98bfd15aff2cb69b817f43f6f6251
Zaboryev Sergey <sergynia88@mail.ru>
2020-11-05 14:48:04 +0300

    Базовый класс моели-представления

---
+++ AppForChildren(MVVM)/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AppForChildren_MVVM_.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChancged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChancged(PropertyName);
+            return true;
+        }
+     }
+}
